47864641fa34c293a8451b2cad5514152c0897a4
CISC181 <bgibbons@udel.edu>
2022-11-11 16:49:36 -0500

    Ready for Telerik/Blazor

---
+++ Client/Pages/School/Course.razor.cs
@@ -0,0 +1,36 @@
+﻿using SNICKERS.Client.Helper;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+using SNICKERS.Shared;
+using SNICKERS.Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Telerik.Blazor.Components;
+
+namespace SNICKERS.Client.Pages.School
+{
+    public partial class Course : SnickersUI
+    {
+        private List<CourseDTO> lstcourse { get; set; }
+
+        private int itmCourse { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            IsLoading = true;
+            await LoadLookupData();
+            IsLoading = false;
+
+
+        }
+        private async Task LoadLookupData()
+        {
+             lstcourse = await Http.GetFromJsonAsync<List<CourseDTO>>("api/Course/GetCourses", options);
+        }
+    }
+}
